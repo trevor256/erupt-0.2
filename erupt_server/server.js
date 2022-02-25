218f31263bbabd389bc6d4a81ae13462b70aa8c7
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 <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>
2022-02-24 22:29:33 -0600

    mongodb func added

--- erupt_server/server.js
+++ erupt_server/server.js
@@ -5,9 +5,49 @@ let ws = require('ws')
 let PORT = 6000
 let server = new ws.Server({ port: PORT })
 
+//mongoDB server
+const {MongoClient} = require('mongodb');
 
-               //server connects//
-              //===============////
+async function main(){
+  const uri = "mongodb+srv://user:pass@erupt.dytny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // makes a new write
+        await createListing(client, {
+          name: "test",
+          summary: "jerk",
+          beds: 12
+        })
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function createListing(client, newlisting) {
+  const result = await client.db("sample").collection("listing")
+  .insertOne(newlisting);
+
+  console.log(`new listing: ${result.insertedId}`);
+}
+
+async function listDatabases(client) {
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+               //server connects to godot client//
+              //===============//
 
 server.on('connection', server => {
 console.log("server running at port", PORT)
