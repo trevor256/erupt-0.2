f074f41b9a80f63faea7da90cfdadf9ffded1375
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 <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>
2022-03-10 20:41:21 -0600

    removed extra server code

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # erupt
-Godot game using ws &amp; nodejs for server communication 
+Godot game using ws &amp; nodejs for server communication
+
+
+1. First item
+2. Second item
+3. Third item
+4. Fourth item
 
 using mongoDB for database
+<span style="color:blue">some *blue* text</span>.
---
+++ erupt_server/1es.js
@@ -0,0 +1,80 @@
+let http = require('http');
+let fs = require('fs');
+let path = require('path');
+
+let ws = require('ws')
+let PORT = 6000
+let server = new ws.Server({ port: PORT })
+
+const {MongoClient} = require('mongodb');
+
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './Erupt.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8080);
+
+
+
+
+// use web sockets to create the connection {npm install ws},
+//server is equal to new server at PORT
+
+
+        //==Godot server==//
+server.on('connection', server => {
+console.log("server running at port", PORT)
+
+//data reseved
+  server.on('message', message => {
+    let datar = JSON.parse(message);
+    console.log(datar, new Date());
+});
+
+//data sent
+  server.send(JSON.stringify({"server":"connected"}));
+});
